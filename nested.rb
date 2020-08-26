8a8c39129f08b48d45a3b29675ba5a701f9889d5
Rodney Smith II <r_smith3@hotmail.com>
2020-08-26 23:11:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,6 +68,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+    },
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,7 +85,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+ programmer_hash
 end
 
 def changing_alan
